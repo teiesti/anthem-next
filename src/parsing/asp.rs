fc399d415c224eaf6a27c4b783b5fa47b94b9a80
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2023-03-31 23:44:08 +0200

    Enhance the glue code for the parser tests

--- src/parsing/asp.rs
+++ src/parsing/asp.rs
@@ -6,67 +6,44 @@ pub struct Parser;
 mod tests {
     use {
         super::{Parser, Rule},
-        crate::parsing::CompleteParser as _,
+        crate::parsing::TestedParser,
     };
-
-    fn recognize(rule: Rule, accept: Vec<&str>, reject: Vec<&str>) {
-        for example in accept {
-            assert!(
-                Parser::parse_complete(rule, example).is_ok(),
-                "assertion failed: rule {rule:?} rejects '{example}'"
-            );
-        }
-
-        for example in reject {
-            assert!(
-                Parser::parse_complete(rule, example).is_err(),
-                "assertion failed: rule {rule:?} accepts '{example}'"
-            );
-        }
-    }
 
     #[test]
     fn recognize_infimum() {
-        recognize(Rule::infimum, vec!["#inf", "#infimum"], vec![])
+        Parser::test_rule(Rule::infimum).should_accept(["#inf", "#infimum"]);
     }
 
     #[test]
     fn recognize_supremum() {
-        recognize(Rule::supremum, vec!["#sup", "#supremum"], vec![])
+        Parser::test_rule(Rule::supremum).should_accept(["#sup", "#supremum"]);
     }
 
     #[test]
     fn recognize_numeral() {
-        recognize(
-            Rule::numeral,
-            vec!["0", "1", "42", "4711"],
-            vec!["a", "A", "4 2", "00"],
-        )
+        Parser::test_rule(Rule::numeral)
+            .should_accept(["0", "1", "42", "4711"])
+            .should_reject(["a", "A", "4 2", "00"]);
     }
 
     #[test]
     fn recognize_constant() {
-        recognize(
-            Rule::constant,
-            vec!["a", "aa", "aA", "_a", "'a", "_'x'_'x'_", "noto"],
-            vec!["A", "1", "a a", "a-a", "'", "not"],
-        )
+        Parser::test_rule(Rule::constant)
+            .should_accept(["a", "aa", "aA", "_a", "'a", "_'x'_'x'_", "noto"])
+            .should_reject(["A", "1", "a a", "a-a", "'", "not"]);
     }
 
     #[test]
     fn recognize_variable() {
-        recognize(
-            Rule::variable,
-            vec!["A", "AA", "Aa", "_A", "'A", "_'X'_'X'_"],
-            vec!["a", "1", "A A", "A-A", "'"],
-        )
+        Parser::test_rule(Rule::variable)
+            .should_accept(["A", "AA", "Aa", "_A", "'A", "_'X'_'X'_"])
+            .should_reject(["a", "1", "A A", "A-A", "'"]);
     }
 
     #[test]
     fn recognize_term() {
-        recognize(
-            Rule::term,
-            vec![
+        Parser::test_rule(Rule::term)
+            .should_accept([
                 "#inf",
                 "1",
                 "a",
@@ -88,8 +65,8 @@ mod tests {
                 "((1 + 2) - 3) * 4",
                 "2 * (1..3)",
                 "1..3..2",
-            ],
-            vec![
+            ])
+            .should_reject([
                 "1-",
                 "1 +",
                 "+ 1",
@@ -98,8 +75,7 @@ mod tests {
                 "(1 + a",
                 "1 + a)",
                 "(1 (+ a +) 1)",
-            ],
-        )
+            ]);
     }
 
     // TODO Tobias: Add tests for the remaining syntax of ASP
--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -1,10 +1,13 @@
 pub mod asp;
 pub mod fol;
 
-use pest::{
-    error::{Error, ErrorVariant},
-    iterators::Pairs,
-    Parser, Position, RuleType,
+use {
+    pest::{
+        error::{Error, ErrorVariant},
+        iterators::Pairs,
+        Parser, Position, RuleType,
+    },
+    std::marker::PhantomData,
 };
 
 pub trait CompleteParser<R: RuleType>: Parser<R> {
@@ -25,3 +28,46 @@ pub trait CompleteParser<R: RuleType>: Parser<R> {
 }
 
 impl<P: Parser<R>, R: RuleType> CompleteParser<R> for P {}
+
+pub trait TestedParser<R: RuleType>: CompleteParser<R> {
+    fn test_rule(rule: R) -> TestedRule<Self, R>
+    where
+        Self: Sized,
+    {
+        TestedRule {
+            parser: PhantomData,
+            rule,
+        }
+    }
+}
+
+impl<P: CompleteParser<R>, R: RuleType> TestedParser<R> for P {}
+
+pub struct TestedRule<P: CompleteParser<R>, R: RuleType> {
+    parser: PhantomData<P>,
+    rule: R,
+}
+
+impl<P: CompleteParser<R>, R: RuleType> TestedRule<P, R> {
+    pub fn should_accept<'a>(&self, examples: impl IntoIterator<Item = &'a str>) -> &Self {
+        for example in examples {
+            assert!(
+                P::parse_complete(self.rule, example).is_ok(),
+                "assertion failed: rule {:?} rejects '{example}'",
+                self.rule
+            );
+        }
+        &self
+    }
+
+    pub fn should_reject<'a>(&self, examples: impl IntoIterator<Item = &'a str>) -> &Self {
+        for example in examples {
+            assert!(
+                P::parse_complete(self.rule, example).is_err(),
+                "assertion failed: rule {:?} accepts '{example}'",
+                self.rule
+            );
+        }
+        &self
+    }
+}
