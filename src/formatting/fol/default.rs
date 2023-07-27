29be34a94dffc1393fd666e3f4073328e35b6feb
ZachJHansen <zachhansen@unomaha.edu>
2023-07-27 14:51:11 -0500

    Adding primitives to FOL syntax tree

--- src/formatting/fol/default.rs
+++ src/formatting/fol/default.rs
@@ -1,1 +1,17 @@
+use {
+    crate::syntax_tree::{fol::Primitive, Node},
+    std::fmt::{self, Display, Formatter},
+};
+
+pub struct Format<'a, N: Node>(pub &'a N);
+
+impl Display for Format<'_, Primitive> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            Primitive::Infimum => write!(f, "#inf"),
+            Primitive::Supremum => write!(f, "#sup"),
+        }
+    }
+}
+
 // TODO Zach: Implement the default formatting for first-order logic here
--- src/syntax_tree/fol.rs
+++ src/syntax_tree/fol.rs
@@ -1,1 +1,11 @@
+use super::Node;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Primitive {
+    Infimum,
+    Supremum,
+}
+
+impl Node for Primitive {}
+
 // TODO Zach: Implement the abstract syntax tree for first-order logic here
