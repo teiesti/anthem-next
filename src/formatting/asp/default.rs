d5ad7e18858e47bb59948cea9d3c6425d4911998
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2023-07-27 18:54:25 +0200

    Implement the ASP constant as an example

--- src/formatting/asp/default.rs
+++ src/formatting/asp/default.rs
@@ -1,1 +1,18 @@
-// TODO Tobias: Implement the default formatting for ASP here
+use {
+    crate::syntax_tree::{asp::Constant, Node},
+    std::fmt::{self, Display, Formatter},
+};
+pub struct Format<'a, N: Node>(pub &'a N);
+
+impl Display for Format<'_, Constant> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            Constant::Infimum => write!(f, "#inf"),
+            Constant::Integer(n) => write!(f, "{n}"),
+            Constant::Symbol(s) => write!(f, "{s}"),
+            Constant::Supremum => write!(f, "#sup"),
+        }
+    }
+}
+
+// TODO Tobias: Continue implementing the default formatting for ASP here
--- src/parsing/asp/pest.rs
+++ src/parsing/asp/pest.rs
@@ -1,1 +1,30 @@
-// TODO Tobias: Implement the pest parsing for ASP here
+use crate::{parsing::PestParser, syntax_tree::asp::Constant};
+
+mod internal {
+    #[derive(pest_derive::Parser)]
+    #[grammar = "parsing/asp/grammar.pest"]
+    pub struct Parser;
+}
+
+pub struct ConstantParser;
+
+impl PestParser for ConstantParser {
+    type Node = Constant;
+
+    type InternalParser = internal::Parser;
+    type Rule = internal::Rule;
+    const RULE: internal::Rule = internal::Rule::constant;
+
+    fn translate_pair(pair: pest::iterators::Pair<'_, Self::Rule>) -> Self::Node {
+        match pair.as_rule() {
+            internal::Rule::constant => Self::translate_pairs(pair.into_inner()),
+            internal::Rule::infimum => Constant::Infimum,
+            internal::Rule::integer => Constant::Integer(pair.as_str().parse().unwrap()),
+            internal::Rule::symbol => Constant::Symbol(pair.as_str().into()),
+            internal::Rule::supremum => Constant::Supremum,
+            _ => Self::report_unexpected_pair(pair),
+        }
+    }
+}
+
+// TODO Tobias: Continue implementing pest parsing for ASP here
