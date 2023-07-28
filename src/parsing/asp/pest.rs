b0d6a24f7458e0c5a3cb439c847713f83a085ed7
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2023-07-28 20:15:30 +0200

    Add tests for parsing unary and binary operator as well as terms

--- src/parsing/asp/pest.rs
+++ src/parsing/asp/pest.rs
@@ -138,10 +138,12 @@ impl PestParser for TermParser {
 #[cfg(test)]
 mod tests {
     use {
-        super::{ConstantParser, VariableParser},
+        super::{
+            BinaryOperatorParser, ConstantParser, TermParser, UnaryOperatorParser, VariableParser,
+        },
         crate::{
             parsing::TestedParser,
-            syntax_tree::asp::{Constant, Variable},
+            syntax_tree::asp::{BinaryOperator, Constant, Term, UnaryOperator, Variable},
         },
     };
 
@@ -184,6 +186,223 @@ mod tests {
                 ("_'X'_'X'_", Variable::Named("_'X'_'X'_".into())),
             ])
             .should_reject(["a", "1", "A A", "A-A", "'", "-A"]);
+    }
+
+    #[test]
+    fn parse_unary_operator() {
+        UnaryOperatorParser.should_parse_into([("-", UnaryOperator::Negative)]);
+    }
+
+    #[test]
+    fn parse_binary_operator() {
+        BinaryOperatorParser.should_parse_into([
+            ("+", BinaryOperator::Add),
+            ("-", BinaryOperator::Subtract),
+            ("*", BinaryOperator::Multiply),
+            ("/", BinaryOperator::Divide),
+            ("\\", BinaryOperator::Modulo),
+            ("..", BinaryOperator::Interval),
+        ]);
+    }
+
+    #[test]
+    fn parse_term() {
+        TermParser
+            .should_parse_into([
+                ("#inf", Term::Constant(Constant::Infimum)),
+                ("#sup", Term::Constant(Constant::Supremum)),
+                ("1", Term::Constant(Constant::Integer(1))),
+                ("(1)", Term::Constant(Constant::Integer(1))),
+                ("-1", Term::Constant(Constant::Integer(-1))),
+                (
+                    "-(1)",
+                    Term::UnaryOperation {
+                        op: UnaryOperator::Negative,
+                        arg: Term::Constant(Constant::Integer(1)).into(),
+                    },
+                ),
+                (
+                    "--1",
+                    Term::UnaryOperation {
+                        op: UnaryOperator::Negative,
+                        arg: Term::Constant(Constant::Integer(-1)).into(),
+                    },
+                ),
+                (
+                    "1 + 2",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Add,
+                        lhs: Term::Constant(Constant::Integer(1)).into(),
+                        rhs: Term::Constant(Constant::Integer(2)).into(),
+                    },
+                ),
+                (
+                    "1..2",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Interval,
+                        lhs: Term::Constant(Constant::Integer(1)).into(),
+                        rhs: Term::Constant(Constant::Integer(2)).into(),
+                    },
+                ),
+                ("a", Term::Constant(Constant::Symbol("a".into()))),
+                ("(a)", Term::Constant(Constant::Symbol("a".into()))),
+                (
+                    "-a",
+                    Term::UnaryOperation {
+                        op: UnaryOperator::Negative,
+                        arg: Term::Constant(Constant::Symbol("a".into())).into(),
+                    },
+                ),
+                (
+                    "-(a)",
+                    Term::UnaryOperation {
+                        op: UnaryOperator::Negative,
+                        arg: Term::Constant(Constant::Symbol("a".into())).into(),
+                    },
+                ),
+                (
+                    "--a",
+                    Term::UnaryOperation {
+                        op: UnaryOperator::Negative,
+                        arg: Term::UnaryOperation {
+                            op: UnaryOperator::Negative,
+                            arg: Term::Constant(Constant::Symbol("a".into())).into(),
+                        }
+                        .into(),
+                    },
+                ),
+                (
+                    "1 + a",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Add,
+                        lhs: Term::Constant(Constant::Integer(1)).into(),
+                        rhs: Term::Constant(Constant::Symbol("a".into())).into(),
+                    },
+                ),
+                (
+                    "1..a",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Interval,
+                        lhs: Term::Constant(Constant::Integer(1)).into(),
+                        rhs: Term::Constant(Constant::Symbol("a".into())).into(),
+                    },
+                ),
+                ("A", Term::Variable(Variable::Named("A".into()))),
+                ("(A)", Term::Variable(Variable::Named("A".into()))),
+                (
+                    "-A",
+                    Term::UnaryOperation {
+                        op: UnaryOperator::Negative,
+                        arg: Term::Variable(Variable::Named("A".into())).into(),
+                    },
+                ),
+                (
+                    "-(A)",
+                    Term::UnaryOperation {
+                        op: UnaryOperator::Negative,
+                        arg: Term::Variable(Variable::Named("A".into())).into(),
+                    },
+                ),
+                (
+                    "--A",
+                    Term::UnaryOperation {
+                        op: UnaryOperator::Negative,
+                        arg: Term::UnaryOperation {
+                            op: UnaryOperator::Negative,
+                            arg: Term::Variable(Variable::Named("A".into())).into(),
+                        }
+                        .into(),
+                    },
+                ),
+                (
+                    "1 + A",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Add,
+                        lhs: Term::Constant(Constant::Integer(1)).into(),
+                        rhs: Term::Variable(Variable::Named("A".into())).into(),
+                    },
+                ),
+                (
+                    "1..A",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Interval,
+                        lhs: Term::Constant(Constant::Integer(1)).into(),
+                        rhs: Term::Variable(Variable::Named("A".into())).into(),
+                    },
+                ),
+                (
+                    "(1 + A) * (1 - a)",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Multiply,
+                        lhs: Term::BinaryOperation {
+                            op: BinaryOperator::Add,
+                            lhs: Term::Constant(Constant::Integer(1)).into(),
+                            rhs: Term::Variable(Variable::Named("A".into())).into(),
+                        }
+                        .into(),
+                        rhs: Term::BinaryOperation {
+                            op: BinaryOperator::Subtract,
+                            lhs: Term::Constant(Constant::Integer(1)).into(),
+                            rhs: Term::Constant(Constant::Symbol("a".into())).into(),
+                        }
+                        .into(),
+                    },
+                ),
+                (
+                    "((1 + 2) - 3) * 4",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Multiply,
+                        lhs: Term::BinaryOperation {
+                            op: BinaryOperator::Subtract,
+                            lhs: Term::BinaryOperation {
+                                op: BinaryOperator::Add,
+                                lhs: Term::Constant(Constant::Integer(1)).into(),
+                                rhs: Term::Constant(Constant::Integer(2)).into(),
+                            }
+                            .into(),
+                            rhs: Term::Constant(Constant::Integer(3)).into(),
+                        }
+                        .into(),
+                        rhs: Term::Constant(Constant::Integer(4)).into(),
+                    },
+                ),
+                (
+                    "2 * (1..3)",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Multiply,
+                        lhs: Term::Constant(Constant::Integer(2)).into(),
+                        rhs: Term::BinaryOperation {
+                            op: BinaryOperator::Interval,
+                            lhs: Term::Constant(Constant::Integer(1)).into(),
+                            rhs: Term::Constant(Constant::Integer(3)).into(),
+                        }
+                        .into(),
+                    },
+                ),
+                (
+                    "1..3..2",
+                    Term::BinaryOperation {
+                        op: BinaryOperator::Interval,
+                        lhs: Term::BinaryOperation {
+                            op: BinaryOperator::Interval,
+                            lhs: Term::Constant(Constant::Integer(1)).into(),
+                            rhs: Term::Constant(Constant::Integer(3)).into(),
+                        }
+                        .into(),
+                        rhs: Term::Constant(Constant::Integer(2)).into(),
+                    },
+                ),
+            ])
+            .should_reject([
+                "1-",
+                "1 +",
+                "+ 1",
+                "1..",
+                "..1",
+                "(1 + a",
+                "1 + a)",
+                "(1 (+ a +) 1)",
+            ]);
     }
 
     // TODO Tobias: Add tests for the remaining parsers
