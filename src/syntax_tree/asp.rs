2dd47a4f5ae0da05fb36fdcbc375b6bf5c58032a
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2023-07-28 19:28:39 +0200

    Add terms to the ASP syntax tree
    This includes the parser but neither tests nor a formatter.

--- src/syntax_tree/asp.rs
+++ src/syntax_tree/asp.rs
@@ -37,4 +37,21 @@ pub enum BinaryOperator {
 
 impl Node for BinaryOperator {}
 
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Term {
+    Constant(Constant),
+    Variable(Variable),
+    UnaryOperation {
+        op: UnaryOperator,
+        arg: Box<Term>,
+    },
+    BinaryOperation {
+        op: BinaryOperator,
+        lhs: Box<Term>,
+        rhs: Box<Term>,
+    },
+}
+
+impl Node for Term {}
+
 // TODO Tobias: Continue implementing the abstract syntax tree for ASP here
