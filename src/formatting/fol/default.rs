99e0fb7ecac36bef644208d80ad059e0829b44df
ZachJHansen <zachansen62@gmail.com>
2023-07-28 10:27:56 -0500

    Adding basic integer terms, wip

--- src/formatting/fol/default.rs
+++ src/formatting/fol/default.rs
@@ -1,16 +1,25 @@
 use {
-    crate::syntax_tree::{fol::Primitive, Node},
+    crate::syntax_tree::{Node, fol::BasicIntegerTerm, fol::IntegerTerm},
     std::fmt::{self, Display, Formatter},
 };
 
 pub struct Format<'a, N: Node>(pub &'a N);
 
-impl Display for Format<'_, Primitive> {
+impl Display for Format<'_, BasicIntegerTerm> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self.0 {
-            Primitive::Infimum => write!(f, "#inf"),
-            Primitive::Supremum => write!(f, "#sup"),
+            BasicIntegerTerm::Infimum => write!(f, "#inf"),
+            BasicIntegerTerm::Supremum => write!(f, "#sup"),
+            BasicIntegerTerm::Numeral(n) => write!(f, "{n}"),
+            BasicIntegerTerm::IntegerVariable(s) => write!(f, "{s}"),
         }
+    }
+}
+
+impl Display for Format<'_, IntegerTerm> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "todo\n");
+        todo();
     }
 }
 
--- src/syntax_tree/fol.rs
+++ src/syntax_tree/fol.rs
@@ -1,11 +1,41 @@
 use super::Node;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub enum Primitive {
-    Infimum,
-    Supremum,
+pub struct UnaryOperator {
+    UnaryMinus: String,
 }
 
-impl Node for Primitive {}
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum BinaryOperator {
+    Add,
+    Subtract,
+    Multiply,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum BasicIntegerTerm {
+    Infimum,
+    Supremum,
+    Numeral(isize),
+    IntegerVariable(String),
+}
+
+impl Node for BasicIntegerTerm {}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum IntegerTerm {
+    BinaryOperation {
+        op: BinaryOperator,
+        lhs: Box<IntegerTerm>,
+        rhs: Box<IntegerTerm>,
+    },
+    UnaryOperation {
+        op: UnaryOperator,
+        arg: Box<IntegerTerm>,
+    },
+    BasicIntegerTerm,
+}
+
+impl Node for IntegerTerm {}
 
 // TODO Zach: Continue implementing the abstract syntax tree for first-order logic here
