334d52f6aa7d20275b900fc5b9c9e65e0e3f5309
ZachJHansen <zachansen62@gmail.com>
2023-07-29 16:36:00 -0500

    Adding integer terms, general terms to syntax tree - integer terms failing checks

--- src/syntax_tree/fol.rs
+++ src/syntax_tree/fol.rs
@@ -29,17 +29,26 @@ impl Node for BasicIntegerTerm {}
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum IntegerTerm {
     BasicIntegerTerm(BasicIntegerTerm),
+    UnaryOperation {
+        op: UnaryOperator,
+        arg: Box<IntegerTerm>,
+    },
     BinaryOperation {
         op: BinaryOperator,
         lhs: Box<IntegerTerm>,
         rhs: Box<IntegerTerm>,
     },
-    UnaryOperation {
-        op: UnaryOperator,
-        arg: Box<IntegerTerm>,
-    },
 }
 
 impl Node for IntegerTerm {}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum GeneralTerm {
+    Symbol(String),
+    GeneralVariable(String),
+    IntegerTerm(IntegerTerm),
+}
+
+impl Node for GeneralTerm {}
 
 // TODO Zach: Continue implementing the abstract syntax tree for first-order logic here
