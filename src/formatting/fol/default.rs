0956fa1345f133ef505e42dd2b22b3e52eae48b1
ZachJHansen <zachansen62@gmail.com>
2023-07-29 16:36:00 -0500

    Adding integer terms, general terms to syntax tree - integer terms failing checks

--- src/formatting/fol/default.rs
+++ src/formatting/fol/default.rs
@@ -1,9 +1,18 @@
 use {
-    crate::syntax_tree::{Node, fol::BasicIntegerTerm, fol::IntegerTerm},
+    crate::syntax_tree::Node,
+    crate::syntax_tree::fol::{UnaryOperator, BinaryOperator, BasicIntegerTerm, IntegerTerm, GeneralTerm},
     std::fmt::{self, Display, Formatter},
 };
 
 pub struct Format<'a, N: Node>(pub &'a N);
+
+impl Display for Format<'_, UnaryOperator> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self.0 {
+            UnaryOperator::Negative => write!(f, "-"),
+        }
+    }
+}
 
 impl Display for Format<'_, BasicIntegerTerm> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -18,9 +27,18 @@ impl Display for Format<'_, BasicIntegerTerm> {
 
 impl Display for Format<'_, IntegerTerm> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "todo\n");
+        match self.0 {
+            IntegerTerm::BasicIntegerTerm(t) => write!(f,"temp"),
+            _ => write!(f, "temp2"),
+        }
+    }
+}
+
+impl Display for Format<'_, GeneralTerm> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         todo!();
     }
 }
+
 
 // TODO Zach: Implement the default formatting for first-order logic here
--- src/parsing/fol/pest.rs
+++ src/parsing/fol/pest.rs
@@ -1,9 +1,62 @@
-use crate::{parsing::PestParser, syntax_tree::fol::BasicIntegerTerm, syntax_tree::fol::IntegerTerm};
+use crate::{
+    parsing::PestParser,
+    syntax_tree::fol::{UnaryOperator, BinaryOperator, BasicIntegerTerm, IntegerTerm, GeneralTerm},
+};
 
 mod internal {
+    use pest::pratt_parser::PrattParser;
+
     #[derive(pest_derive::Parser)]
     #[grammar = "parsing/fol/grammar.pest"]
     pub struct Parser;
+
+    lazy_static::lazy_static! {
+        pub static ref PRATT_PARSER: PrattParser<Rule> = {
+            use pest::pratt_parser::{Assoc::*, Op};
+            use Rule::*;
+
+            PrattParser::new()
+                .op(Op::infix(add, Left) | Op::infix(subtract, Left))
+                .op(Op::infix(multiply, Left))
+                .op(Op::prefix(negative))
+        };
+    }
+}
+
+pub struct UnaryOperatorParser;
+
+impl PestParser for UnaryOperatorParser {
+    type Node = UnaryOperator;
+
+    type InternalParser = internal::Parser;
+    type Rule = internal::Rule;
+    const RULE: internal::Rule = internal::Rule::unary_operator;
+
+    fn translate_pair(pair: pest::iterators::Pair<'_, Self::Rule>) -> Self::Node {
+        match pair.as_rule() {          // No need for translate_pairs into_inner since triggering rule is silent
+            internal::Rule::negative => UnaryOperator::Negative,
+            _ => Self::report_unexpected_pair(pair),
+        }
+    }
+}
+
+pub struct BinaryOperatorParser;
+
+impl PestParser for BinaryOperatorParser {
+    type Node = BinaryOperator;
+
+    type InternalParser = internal::Parser;
+    type Rule = internal::Rule;
+    const RULE: internal::Rule = internal::Rule::binary_operator;
+
+    fn translate_pair(pair: pest::iterators::Pair<'_, Self::Rule>) -> Self::Node {
+        match pair.as_rule() {
+            internal::Rule::add => BinaryOperator::Add,
+            internal::Rule::subtract => BinaryOperator::Subtract,
+            internal::Rule::multiply => BinaryOperator::Multiply,
+            _ => Self::report_unexpected_pair(pair),
+        }
+    }
 }
 
 pub struct BasicIntegerTermParser;
@@ -29,23 +82,6 @@ impl PestParser for BasicIntegerTermParser {
     }
 }
 
-pub struct UnaryOperatorParser;
-
-impl PestParser for UnaryOperatorParser {
-    type Node = UnaryOperator;
-
-    type InternalParser = internal::Parser;
-    type Rule = internal::Rule;
-    const RULE: internal::Rule = internal::Rule::n_term;
-
-    fn translate_pair(pair: pest::iterators::Pair<'_, Self::Rule>) -> Self::Node {
-        match pair.as_rule() {          // No need for translate_pairs into_inner since triggering rule is silent
-            internal::Rule::negative => UnaryOperator::Negative,
-            _ => Self::report_unexpected_pair(pair),
-        }
-    }
-}
-
 pub struct IntegerTermParser;
 
 impl PestParser for IntegerTermParser {
@@ -55,7 +91,44 @@ impl PestParser for IntegerTermParser {
     type Rule = internal::Rule;
     const RULE: internal::Rule = internal::Rule::n_term;
 
+    fn translate_pair(pair: pest::iterators::Pair<'_, Self::Rule>) -> Self::Node {
+        internal::PRATT_PARSER
+            .map_primary(|primary| match primary.as_rule() {
+                internal::Rule::n_term => IntegerTermParser::translate_pair(primary),
+                internal::Rule::n_basic_term => IntegerTerm::BasicIntegerTerm(BasicIntegerTermParser::translate_pair(primary)),
+                _ => Self::report_unexpected_pair(primary),
+            })
+            .map_prefix(|op, arg| IntegerTerm::UnaryOperation {
+                op: UnaryOperatorParser::translate_pair(op),
+                arg: Box::new(arg),
+            })
+            .map_infix(|lhs, op, rhs| IntegerTerm::BinaryOperation {
+                op: BinaryOperatorParser::translate_pair(op),
+                lhs: Box::new(lhs),
+                rhs: Box::new(rhs),
+            })
+            .parse(pair.into_inner())
+    }
+}
 
+pub struct GeneralTermParser;
+
+impl PestParser for GeneralTermParser {
+    type Node = GeneralTerm;
+
+    type InternalParser = internal::Parser;
+    type Rule = internal::Rule;
+    const RULE: internal::Rule = internal::Rule::g_term;
+
+    fn translate_pair(pair: pest::iterators::Pair<'_, Self::Rule>) -> Self::Node {
+        match pair.as_rule() {
+            internal::Rule::g_term => Self::translate_pairs(pair.into_inner()),
+            internal::Rule::symbolic_constant => GeneralTerm::Symbol(pair.as_str().into()),
+            internal::Rule::n_term => GeneralTerm::IntegerTerm(IntegerTermParser::translate_pair(pair)),
+            internal::Rule::g_variable => GeneralTerm::GeneralVariable(pair.into_inner().next().unwrap().as_str().into()),
+            _ => Self::report_unexpected_pair(pair),
+        }
+    }
 }
 
 // TODO Zach: Continue implementing pest parsing for first-order logic here
@@ -63,23 +136,60 @@ impl PestParser for IntegerTermParser {
 #[cfg(test)]
 mod tests {
     use {
-        super::PrimitiveParser,
-        crate::{parsing::TestedParser, syntax_tree::fol::Primitive},
+        super::{
+            UnaryOperatorParser, BinaryOperatorParser, BasicIntegerTermParser, IntegerTermParser,
+        },
+        crate::{
+            parsing::TestedParser,
+            syntax_tree::fol::{UnaryOperator, BinaryOperator, BasicIntegerTerm, IntegerTerm},
+        }
     };
 
     #[test]
-    fn parse_primitive() {
-        PrimitiveParser
+    fn parse_unary_operator() {
+        UnaryOperatorParser.should_parse_into([("-", UnaryOperator::Negative)]);
+    }
+
+    #[test]
+    fn parse_binary_operator() {
+        BinaryOperatorParser.should_parse_into([
+            ("+", BinaryOperator::Add),
+            ("-", BinaryOperator::Subtract),
+            ("*", BinaryOperator::Multiply),
+        ]);
+    }
+
+    #[test]
+    fn parse_basic_integer_term() {
+        BasicIntegerTermParser
             .should_parse_into([
-                ("#inf", Primitive::Infimum),
-                // ("#infimum", Primitive::Infimum),
-                ("#sup", Primitive::Supremum),
-                // ("#supremum", Primitive::Supremum),
+                ("#inf", BasicIntegerTerm::Infimum),
+                ("#sup", BasicIntegerTerm::Supremum),
+                ("0", BasicIntegerTerm::Numeral(0)),
+                ("1", BasicIntegerTerm::Numeral(1)),
+                ("-1", BasicIntegerTerm::Numeral(-1)),
+                ("-48", BasicIntegerTerm::Numeral(-48)),
+                ("301", BasicIntegerTerm::Numeral(301)),
             ])
             .should_reject([
-                // TODO Zach: Add examples
+                "00", "-0", "--1", "#", "#infi", "#supa", "_", "1_"
             ]);
     }
 
-    // TODO Zach: Add tests for the remaining parsers
+    #[test]
+    fn parse_integer_term() {
+        IntegerTermParser
+            .should_parse_into([
+                ("#inf", IntegerTerm::BasicIntegerTerm(BasicIntegerTerm::Infimum)),
+                ("#sup", IntegerTerm::BasicIntegerTerm(BasicIntegerTerm::Supremum)),
+                ("0", IntegerTerm::BasicIntegerTerm(BasicIntegerTerm::Numeral(0))),
+                ("1", IntegerTerm::BasicIntegerTerm(BasicIntegerTerm::Numeral(1))),
+                ("-1", IntegerTerm::BasicIntegerTerm(BasicIntegerTerm::Numeral(-1))),
+                ("(-48)", IntegerTerm::BasicIntegerTerm(BasicIntegerTerm::Numeral(-48))),
+                ("(301)", IntegerTerm::BasicIntegerTerm(BasicIntegerTerm::Numeral(301))),
+            ])
+            .should_reject([
+                "00", "-0", "--1", "#", "#infi", "#supa", "_", "1_", "(1"
+            ]);
+    }
 }
