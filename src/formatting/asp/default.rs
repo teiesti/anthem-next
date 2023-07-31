91ec90ee5a511e2bf1300c36b2ad159998c7a2af
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2023-07-31 20:53:11 +0200

    Test the formatting of constants, variables and operators

--- src/formatting/asp/default.rs
+++ src/formatting/asp/default.rs
@@ -1,6 +1,6 @@
 use {
     crate::syntax_tree::{
-        asp::{Constant, Variable, UnaryOperator, BinaryOperator, Term},
+        asp::{BinaryOperator, Constant, Term, UnaryOperator, Variable},
         Node,
     },
     std::fmt::{self, Display, Formatter},
@@ -44,7 +44,7 @@ impl Display for Format<'_, BinaryOperator> {
             BinaryOperator::Multiply => write!(f, "*"),
             BinaryOperator::Divide => write!(f, "/"),
             BinaryOperator::Modulo => write!(f, "\\"),
-            BinaryOperator::Interval => write!(f, "*"),
+            BinaryOperator::Interval => write!(f, ".."),
         }
     }
 }
@@ -56,3 +56,42 @@ impl Display for Format<'_, Term> {
 }
 
 // TODO Tobias: Continue implementing the default formatting for ASP here
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        formatting::asp::default::Format,
+        syntax_tree::asp::{BinaryOperator, Constant, UnaryOperator, Variable},
+    };
+
+    #[test]
+    fn format_constant() {
+        assert_eq!(Format(&Constant::Infimum).to_string(), "#inf");
+        assert_eq!(Format(&Constant::Integer(-1)).to_string(), "-1");
+        assert_eq!(Format(&Constant::Integer(0)).to_string(), "0");
+        assert_eq!(Format(&Constant::Integer(42)).to_string(), "42");
+        assert_eq!(Format(&Constant::Symbol("a".into())).to_string(), "a");
+        assert_eq!(Format(&Constant::Supremum).to_string(), "#sup");
+    }
+
+    #[test]
+    fn format_variable() {
+        assert_eq!(Format(&Variable::Anonymous).to_string(), "_");
+        assert_eq!(Format(&Variable::Named("A".into())).to_string(), "A");
+    }
+
+    #[test]
+    fn format_unary_operator() {
+        assert_eq!(Format(&UnaryOperator::Negative).to_string(), "-");
+    }
+
+    #[test]
+    fn format_binary_operator() {
+        assert_eq!(Format(&BinaryOperator::Add).to_string(), "+");
+        assert_eq!(Format(&BinaryOperator::Subtract).to_string(), "-");
+        assert_eq!(Format(&BinaryOperator::Multiply).to_string(), "*");
+        assert_eq!(Format(&BinaryOperator::Divide).to_string(), "/");
+        assert_eq!(Format(&BinaryOperator::Modulo).to_string(), "\\");
+        assert_eq!(Format(&BinaryOperator::Interval).to_string(), "..");
+    }
+}
