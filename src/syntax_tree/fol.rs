5a520ff5b7f2e4e5efd8c4bc6f57b35c201741ce
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2023-11-18 05:32:14 +0100

    Add UnboxedFormula as a convenience type

--- src/syntax_tree/fol.rs
+++ src/syntax_tree/fol.rs
@@ -259,6 +259,83 @@ impl Formula {
             Formula::QuantifiedFormula { formula, .. } => formula.variables(),
         }
     }
+
+    pub fn unbox(self) -> UnboxedFormula {
+        match self {
+            Self::AtomicFormula(f) => UnboxedFormula::AtomicFormula(f),
+            Self::UnaryFormula {
+                connective,
+                formula,
+            } => UnboxedFormula::UnaryFormula {
+                connective,
+                formula: *formula,
+            },
+            Self::BinaryFormula {
+                connective,
+                lhs,
+                rhs,
+            } => UnboxedFormula::BinaryFormula {
+                connective,
+                lhs: *lhs,
+                rhs: *rhs,
+            },
+            Self::QuantifiedFormula {
+                quantification,
+                formula,
+            } => UnboxedFormula::QuantifiedFormula {
+                quantification,
+                formula: *formula,
+            },
+        }
+    }
+}
+
+pub enum UnboxedFormula {
+    AtomicFormula(AtomicFormula),
+    UnaryFormula {
+        connective: UnaryConnective,
+        formula: Formula,
+    },
+    BinaryFormula {
+        connective: BinaryConnective,
+        lhs: Formula,
+        rhs: Formula,
+    },
+    QuantifiedFormula {
+        quantification: Quantification,
+        formula: Formula,
+    },
+}
+
+impl UnboxedFormula {
+    pub fn rebox(self) -> Formula {
+        match self {
+            Self::AtomicFormula(f) => Formula::AtomicFormula(f),
+            Self::UnaryFormula {
+                connective,
+                formula,
+            } => Formula::UnaryFormula {
+                connective,
+                formula: Box::new(formula),
+            },
+            Self::BinaryFormula {
+                connective,
+                lhs,
+                rhs,
+            } => Formula::BinaryFormula {
+                connective,
+                lhs: Box::new(lhs),
+                rhs: Box::new(rhs),
+            },
+            Self::QuantifiedFormula {
+                quantification,
+                formula,
+            } => Formula::QuantifiedFormula {
+                quantification,
+                formula: Box::new(formula),
+            },
+        }
+    }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
