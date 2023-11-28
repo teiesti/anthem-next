855cf25288f14a70abfbd21986c4e060f189ebf4
ZachJHansen <zachansen62@gmail.com>
2023-11-28 11:53:07 -0600

    Adding total function formula construction function

--- src/translating/tau_star.rs
+++ src/translating/tau_star.rs
@@ -137,6 +137,75 @@ fn construct_equality_formula(term: asp::Term, z: fol::Variable) -> fol::Formula
     }))
 }
 
+// +,-,*
+// exists I J (Z = I op J & val_t1(I) & val_t2(J))
+fn construct_total_function_formula(
+    valti: fol::Formula,
+    valtj: fol::Formula,
+    binop: asp::BinaryOperator,
+    i_var: fol::Variable,
+    j_var: fol::Variable,
+    z: fol::Variable,
+) -> fol::Formula {
+    let i = i_var.name;
+    let j = j_var.name;
+    let z_var_term = match z.sort {
+        fol::Sort::General => fol::GeneralTerm::GeneralVariable(z.name.into()),
+        fol::Sort::Integer => fol::GeneralTerm::IntegerTerm(fol::IntegerTerm::BasicIntegerTerm(
+            fol::BasicIntegerTerm::IntegerVariable(z.name.into()),
+        )),
+    };
+    let zequals = fol::Formula::AtomicFormula(fol::AtomicFormula::Comparison(fol::Comparison {
+        // Z = I binop J
+        term: z_var_term,
+        guards: vec![fol::Guard {
+            relation: fol::Relation::Equal,
+            term: fol::GeneralTerm::IntegerTerm(fol::IntegerTerm::BinaryOperation {
+                op: match binop {
+                    asp::BinaryOperator::Add => fol::BinaryOperator::Add,
+                    asp::BinaryOperator::Subtract => fol::BinaryOperator::Subtract,
+                    asp::BinaryOperator::Multiply => fol::BinaryOperator::Multiply,
+                    _ => panic!(), // More error handling
+                },
+                lhs: fol::IntegerTerm::BasicIntegerTerm(fol::BasicIntegerTerm::IntegerVariable(
+                    i.clone(),
+                ))
+                .into(),
+                rhs: fol::IntegerTerm::BasicIntegerTerm(fol::BasicIntegerTerm::IntegerVariable(
+                    j.clone(),
+                ))
+                .into(),
+            }),
+        }],
+    }));
+    fol::Formula::QuantifiedFormula {
+        quantification: fol::Quantification {
+            quantifier: fol::Quantifier::Exists,
+            variables: vec![
+                fol::Variable {
+                    name: i.into(),
+                    sort: fol::Sort::Integer,
+                },
+                fol::Variable {
+                    name: j.into(),
+                    sort: fol::Sort::Integer,
+                },
+            ],
+        },
+        formula: fol::Formula::BinaryFormula {
+            connective: fol::BinaryConnective::Conjunction,
+            lhs: fol::Formula::BinaryFormula {
+                connective: fol::BinaryConnective::Conjunction,
+                lhs: zequals.into(),
+                rhs: valti.into(),
+            }
+            .into(),
+            rhs: valtj.into(),
+        }
+        .into(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::{conjoin, disjoin};
