c823c76ee18916ff6838a6b31424bf339e02874d
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2024-04-22 20:29:02 +0200

    Implement (most parts of) completion

--- src/translating/completion.rs
+++ src/translating/completion.rs
@@ -1,7 +1,52 @@
-use crate::{
-    convenience::unbox::{fol::UnboxedFormula, Unbox},
-    syntax_tree::fol,
+use {
+    crate::{
+        convenience::unbox::{fol::UnboxedFormula, Unbox},
+        syntax_tree::fol,
+    },
+    indexmap::{map::Entry, IndexMap},
 };
+
+pub fn completion(theory: fol::Theory) -> Option<fol::Theory> {
+    let definitions = definitions(theory)?;
+
+    // TODO: Take care for constraints
+    // TODO: Confirm there are not head mismatches
+
+    let formulas = definitions
+        .into_iter()
+        .map(|(g, f)| {
+            let v = g.variables();
+            fol::Formula::BinaryFormula {
+                connective: fol::BinaryConnective::Equivalence,
+                lhs: fol::Formula::AtomicFormula(g).into(),
+                rhs: fol::Formula::disjoin(f.into_iter().map(|f_i| {
+                    let variables = f_i.free_variables().difference(&v).cloned().collect();
+                    f_i.quantify(fol::Quantifier::Exists, variables)
+                }))
+                .into(),
+            }
+            .quantify(fol::Quantifier::Forall, v.into_iter().collect())
+        })
+        .collect();
+
+    Some(fol::Theory { formulas })
+}
+
+fn definitions(theory: fol::Theory) -> Option<IndexMap<fol::AtomicFormula, Vec<fol::Formula>>> {
+    let mut result: IndexMap<_, Vec<fol::Formula>> = IndexMap::new();
+    for formula in theory.formulas {
+        let (f, g) = split(formula)?;
+        match result.entry(g) {
+            Entry::Occupied(mut e) => {
+                e.get_mut().push(f);
+            }
+            Entry::Vacant(e) => {
+                e.insert(vec![f]);
+            }
+        };
+    }
+    Some(result)
+}
 
 pub fn is_completable_formula(formula: fol::Formula) -> bool {
     split(formula).is_some()
