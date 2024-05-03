e862ae1cf9d69de0e947c4eb6b7148abc5c31d64
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2024-05-03 19:20:57 +0200

    Break external equivalence decomposition into stages

--- src/verifying/task/external_equivalence.rs
+++ src/verifying/task/external_equivalence.rs
@@ -2,13 +2,15 @@ use {
     crate::{
         command_line::Decomposition,
         syntax_tree::{asp, fol},
-        verifying::{problem::Problem, task::Task},
+        verifying::{problem::{self, Problem}, task::Task},
     },
     either::Either,
-    indexmap::IndexSet,
-    std::fmt::Display,
     thiserror::Error,
 };
+
+#[derive(Error, Debug)]
+pub enum ExternalEquivalenceTaskError {
+}
 
 #[derive(Debug)]
 pub struct ExternalEquivalenceTask {
@@ -22,106 +24,49 @@ pub struct ExternalEquivalenceTask {
     pub break_equivalences: bool,
 }
 
-#[derive(Error, Debug)]
-pub enum ExternalEquivalenceTaskError {
-    InputOutputPredicatesOverlap(Vec<fol::Predicate>),
-    InputPredicateInRuleHead(Vec<fol::Predicate>),
-}
-
-impl Display for ExternalEquivalenceTaskError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ExternalEquivalenceTaskError::InputOutputPredicatesOverlap(predicates) => {
-                write!(
-                    f,
-                    "the following predicates are declared as input and output predicates: "
-                )?;
-
-                let mut iter = predicates.iter().peekable();
-                for predicate in predicates {
-                    write!(f, "{predicate}")?;
-                    if iter.peek().is_some() {
-                        write!(f, ", ")?;
-                    }
-                }
-
-                writeln!(f)
-            }
-            ExternalEquivalenceTaskError::InputPredicateInRuleHead(predicates) => {
-                write!(
-                    f,
-                    "the following predicates are declared as input and output predicates: "
-                )?;
-
-                let mut iter = predicates.iter().peekable();
-                for predicate in predicates {
-                    write!(f, "{predicate}")?;
-                    if iter.peek().is_some() {
-                        write!(f, ", ")?;
-                    }
-                }
-
-                writeln!(f)
-            }
-        }
-    }
-}
-
-impl ExternalEquivalenceTask {
-    fn ensure_input_and_output_predicates_are_disjoint(
-        &self,
-    ) -> Result<(), ExternalEquivalenceTaskError> {
-        let input_predicates = self.user_guide.input_predicates();
-        let output_predicates = self.user_guide.output_predicates();
-
-        let intersection: Vec<_> = input_predicates
-            .intersection(&output_predicates)
-            .cloned()
-            .collect();
-
-        if intersection.is_empty() {
-            Ok(())
-        } else {
-            Err(ExternalEquivalenceTaskError::InputOutputPredicatesOverlap(
-                intersection,
-            ))
-        }
-    }
-
-    fn ensure_program_heads_do_not_contain_input_predicates(
-        &self,
-    ) -> Result<(), ExternalEquivalenceTaskError> {
-        let input_predicates = self.user_guide.input_predicates();
-        let head_predicates: IndexSet<_> = self
-            .program
-            .head_predicates()
-            .into_iter()
-            .map(fol::Predicate::from)
-            .collect();
-
-        let intersection: Vec<_> = input_predicates
-            .intersection(&head_predicates)
-            .cloned()
-            .collect();
-
-        if intersection.is_empty() {
-            Ok(())
-        } else {
-            Err(ExternalEquivalenceTaskError::InputPredicateInRuleHead(
-                intersection,
-            ))
-        }
-    }
-}
-
 impl Task for ExternalEquivalenceTask {
     type Error = ExternalEquivalenceTaskError;
 
     fn decompose(self) -> Result<Vec<Problem>, Self::Error> {
-        self.ensure_input_and_output_predicates_are_disjoint()?;
-        self.ensure_program_heads_do_not_contain_input_predicates()?;
-        // TODO: Add more error handing
+        // let task: ValidatedExternalEquivalenceTask = todo!();
+        // task.decompose()
+        todo!()
+    }
+}
 
+struct ValidatedExternalEquivalenceTask {
+    pub left: Vec<fol::AnnotatedFormula>,
+    pub right: Vec<fol::AnnotatedFormula>,
+    pub user_guide_assumptions: Vec<fol::AnnotatedFormula>,
+    // pub proof_outline: ProofOutline,
+    pub direction: fol::Direction,
+    pub break_equivalences: bool,
+}
+
+impl Task for ValidatedExternalEquivalenceTask {
+    type Error = ExternalEquivalenceTaskError;
+
+    fn decompose(self) -> Result<Vec<Problem>, Self::Error> {
+        // let task: AssembledExternalEquivalenceTask = todo!();
+        // task.decompose()
+        todo!()
+    }
+}
+
+struct AssembledExternalEquivalenceTask {
+    pub stable_premises: Vec<problem::AnnotatedFormula>,
+    pub forward_premises: Vec<problem::AnnotatedFormula>,
+    pub forward_conclusions: Vec<problem::AnnotatedFormula>,
+    pub backward_premises: Vec<problem::AnnotatedFormula>,
+    pub backward_conclusions: Vec<problem::AnnotatedFormula>,
+    //  pub proof_outline: ProofOutline,
+    pub direction: fol::Direction,
+}
+
+impl Task for AssembledExternalEquivalenceTask {
+    type Error = ExternalEquivalenceTask;
+
+    fn decompose(self) -> Result<Vec<Problem>, Self::Error> {
         todo!()
     }
 }
