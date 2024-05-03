9917082c11ae390b3ed1f4b5a333e4afe73bfb98
ZachJHansen <zachansen62@gmail.com>
2024-05-03 10:08:04 -0500

    wip

--- tests/identity.rs
+++ tests/identity.rs
@@ -16,3 +16,20 @@ fn asp_rule_default_parsing_formatting_identity() {
         );
     }
 }
+
+#[test]
+fn annotated_formula_default_parsing_formatting_identity() {
+    for src in [
+        "assumption(backward)[p_or_q]: forall X ( p(X) or exists N$ ( q(N$) and N$ = X ) )",
+        "spec[covered]: forall Y ( exists X ( covered(Y,X) ) )",
+    ] {
+        let rule: fol::AnnotatedFormula = src.parse().unwrap();
+        let target = format!("{rule}");
+
+        assert_eq!(
+            src.to_string(),
+            target.to_string(),
+            "assertion `left == right` failed:\n left:\n{src}\n right:\n{target}"
+        );
+    }
+}
