79831c646655ed8736e5e6d4c6be43323a010ee2
ZachJHansen <zachansen62@gmail.com>
2024-05-10 14:46:44 -0500

    cleaning up after merge

--- src/verifying/task/external_equivalence.rs
+++ src/verifying/task/external_equivalence.rs
@@ -278,11 +278,9 @@ mod tests {
         let assumption_1: fol::AnnotatedFormula = "assumption: t -> q".parse().unwrap();
         let proof_outline = ProofOutline {
             forward_definitions: vec![],
-            forward_basic_lemmas: vec![],
-            forward_inductive_lemmas: vec![],
             backward_definitions: vec![],
-            backward_basic_lemmas: vec![],
-            backward_inductive_lemmas: vec![],
+            forward_lemmas: vec![],
+            backward_lemmas: vec![],
         };
         let validated = ValidatedExternalEquivalenceTask {
             left,
@@ -342,11 +340,9 @@ mod tests {
         ];
         let proof_outline = ProofOutline {
             forward_definitions: vec![],
-            forward_basic_lemmas: vec![],
-            forward_inductive_lemmas: vec![],
             backward_definitions: vec![],
-            backward_basic_lemmas: vec![],
-            backward_inductive_lemmas: vec![],
+            forward_lemmas: vec![],
+            backward_lemmas: vec![],
         };
 
         let assembled = AssembledExternalEquivalenceTask {
