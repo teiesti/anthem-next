34cb06b8d639f6f3effc1a09fd54a0a46c50f0c2
ZachJHansen <zachansen62@gmail.com>
2024-05-11 09:34:40 -0500

    first draft of decompose function for external equivalence task

--- src/verifying/task/external_equivalence.rs
+++ src/verifying/task/external_equivalence.rs
@@ -1,18 +1,127 @@
 use {
     crate::{
         command_line::Decomposition,
-        syntax_tree::{asp, fol},
+        convenience::apply::Apply,
+        syntax_tree::{
+            asp,
+            fol,
+        },
+        translating::{completion::completion, tau_star::tau_star},
         verifying::{
             problem::{self, AnnotatedFormula, Problem},
-            task::{Task, ProofOutline},
+            task::{ProofOutline, Task},
         },
     },
     either::Either,
+    indexmap::IndexMap,
     thiserror::Error,
 };
 
+// TODO: The following could be much easier with an enum over all types of nodes which implements the apply trait
+trait ReplacePlaceholders {
+    fn replace_placeholders(self, mapping: &IndexMap<String, fol::FunctionConstant>) -> Self;
+}
+
+impl ReplacePlaceholders for fol::Theory {
+    fn replace_placeholders(self, mapping: &IndexMap<String, fol::FunctionConstant>) -> Self {
+        fol::Theory {
+            formulas: self
+                .formulas
+                .into_iter()
+                .map(|f| f.replace_placeholders(mapping))
+                .collect(),
+        }
+    }
+}
+
+impl ReplacePlaceholders for fol::Formula {
+    fn replace_placeholders(self, mapping: &IndexMap<String, fol::FunctionConstant>) -> Self {
+        self.apply(&mut |formula| match formula {
+            fol::Formula::AtomicFormula(a) => {
+                fol::Formula::AtomicFormula(a.replace_placeholders(mapping))
+            }
+            x => x,
+        })
+    }
+}
+
+impl ReplacePlaceholders for fol::AtomicFormula {
+    fn replace_placeholders(self, mapping: &IndexMap<String, fol::FunctionConstant>) -> Self {
+        match self {
+            fol::AtomicFormula::Atom(a) => {
+                fol::AtomicFormula::Atom(a.replace_placeholders(mapping))
+            }
+            fol::AtomicFormula::Comparison(c) => {
+                fol::AtomicFormula::Comparison(c.replace_placeholders(mapping))
+            }
+            x => x,
+        }
+    }
+}
+
+impl ReplacePlaceholders for fol::Atom {
+    fn replace_placeholders(self, mapping: &IndexMap<String, fol::FunctionConstant>) -> Self {
+        fol::Atom {
+            predicate_symbol: self.predicate_symbol,
+            terms: self
+                .terms
+                .into_iter()
+                .map(|t| t.replace_placeholders(mapping))
+                .collect(),
+        }
+    }
+}
+
+impl ReplacePlaceholders for fol::Comparison {
+    fn replace_placeholders(self, mapping: &IndexMap<String, fol::FunctionConstant>) -> Self {
+        fol::Comparison {
+            term: self.term.replace_placeholders(mapping),
+            guards: self
+                .guards
+                .into_iter()
+                .map(|g| g.replace_placeholders(mapping))
+                .collect(),
+        }
+    }
+}
+
+impl ReplacePlaceholders for fol::Guard {
+    fn replace_placeholders(self, mapping: &IndexMap<String, fol::FunctionConstant>) -> Self {
+        fol::Guard {
+            relation: self.relation,
+            term: self.term.replace_placeholders(mapping),
+        }
+    }
+}
+
+impl ReplacePlaceholders for fol::GeneralTerm {
+    fn replace_placeholders(self, mapping: &IndexMap<String, fol::FunctionConstant>) -> Self {
+        match self {
+            fol::GeneralTerm::SymbolicTerm(fol::SymbolicTerm::Symbol(s)) => {
+                if let Some(fc) = mapping.get(&s) {
+                    match fc.sort {
+                        fol::Sort::General => fol::GeneralTerm::FunctionConstant(s),
+                        fol::Sort::Integer => {
+                            fol::GeneralTerm::IntegerTerm(fol::IntegerTerm::FunctionConstant(s))
+                        }
+                        fol::Sort::Symbol => {
+                            fol::GeneralTerm::SymbolicTerm(fol::SymbolicTerm::FunctionConstant(s))
+                        }
+                    }
+                } else {
+                    fol::GeneralTerm::SymbolicTerm(fol::SymbolicTerm::Symbol(s))
+                }
+            }
+            x => x,
+        }
+    }
+}
+
 #[derive(Error, Debug)]
-pub enum ExternalEquivalenceTaskError {}
+pub enum ExternalEquivalenceTaskError {
+    #[error("could not parse the proof outline: {0}")]
+    ProofOutline(String),
+}
 
 #[derive(Debug)]
 pub struct ExternalEquivalenceTask {
@@ -33,11 +142,122 @@ impl Task for ExternalEquivalenceTask {
         //self.ensure_input_and_output_predicates_are_disjoint()?;
         //self.ensure_program_heads_do_not_contain_input_predicates()?;
 
-        let taken_predicates = self.user_guide.input_predicates();
-        let _proof_outline = ProofOutline::construct(self.proof_outline, taken_predicates);
+        // TODO: Ensure there's not a$i and a$g in the user guide
+        let placeholder = self
+            .user_guide
+            .placeholders()
+            .into_iter()
+            .map(|p| (p.name.clone(), p))
+            .collect();
+
+        let public_predicates = self.user_guide.public_predicates();
+
+        // // TODO: Apply simplifications
+        // // TODO: Apply equivalence breaking
+        // // TODO: Rename private predicates
+
+        let head_predicate = |formula: fol::Formula| {
+            let head_formula = *match formula {
+                fol::Formula::BinaryFormula {
+                    connective: fol::BinaryConnective::Equivalence,
+                    lhs,
+                    ..
+                } => lhs,
+                fol::Formula::QuantifiedFormula { formula, .. } => match *formula {
+                    fol::Formula::BinaryFormula {
+                        connective: fol::BinaryConnective::Equivalence,
+                        lhs,
+                        ..
+                    } => lhs,
+                    _ => None?,
+                },
+                _ => None?,
+            };
+
+            Some(
+                match head_formula {
+                    fol::Formula::AtomicFormula(fol::AtomicFormula::Atom(a)) => a,
+                    _ => None?,
+                }
+                .predicate(),
+            )
+        };
+
+        // Translate a first-order theory corresponding to the completion of an ASP program
+        // into a control language specification
+        let control_translate = |theory: fol::Theory| {
+            let mut control_formulas: Vec<fol::AnnotatedFormula> = Vec::new();
+
+            for formula in theory.formulas {
+                match head_predicate(formula.clone()) {
+                    Some(p) => {
+                        if !public_predicates.contains(&p) {
+                            control_formulas.push(fol::AnnotatedFormula {
+                                name: format!("completed_definition_of_{p}"),
+                                role: fol::Role::Assumption,
+                                direction: fol::Direction::Universal,
+                                formula: formula.clone(),
+                            });
+                        } else {
+                            control_formulas.push(fol::AnnotatedFormula {
+                                name: format!("completed_definition_of_{p}"),
+                                role: fol::Role::Spec,
+                                direction: fol::Direction::Universal,
+                                formula: formula.clone(),
+                            });
+                        }
+                    }
+                    _ => control_formulas.push(fol::AnnotatedFormula {
+                        name: "constraint".to_string(),
+                        role: fol::Role::Spec,
+                        direction: fol::Direction::Universal,
+                        formula: formula.clone(),
+                    }),
+                }
+            }
+
+            control_formulas
+        };
+
+        let program = completion(tau_star(self.program.clone()).replace_placeholders(&placeholder))
+            .expect("tau_star did not create a completable theory");
+        let right = control_translate(program);
+
+        let left = match self.specification.clone() {
+            Either::Left(specification) => {
+                let specification =
+                    completion(tau_star(specification).replace_placeholders(&placeholder))
+                        .expect("tau_star did not create a completable theory");
+                control_translate(specification)
+            }
+            Either::Right(specification) => specification.formulas,
+        };
+
+        let mut taken_predicates = self.user_guide.input_predicates();
+        for anf in left.iter() {
+            taken_predicates.extend(anf.formula.predicates());
+        }
+        for anf in right.iter() {
+            taken_predicates.extend(anf.formula.predicates());
+        }
+
+        let proof_outline = match ProofOutline::construct(self.proof_outline, taken_predicates) {
+            Ok(outline) => Ok(outline),
+            Err(e) => Err(ExternalEquivalenceTaskError::ProofOutline(e.to_string())),
+        }?;
+
         // TODO: Add more error handing
 
-        todo!()
+        let validated = ValidatedExternalEquivalenceTask {
+            left,
+            right,
+            user_guide_assumptions: vec![],
+            proof_outline,
+            decomposition: self.decomposition,
+            direction: self.direction,
+            break_equivalences: self.break_equivalences,
+        };
+        validated.decompose()
     }
 }
 
