5e1876c577ecf16aca89e4df2ac2736f652359f2
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2024-08-01 17:39:59 +0200

    Implement decomposing AssembledExternalEquivalenceTasks into Problems

--- src/verifying/task/external_equivalence.rs
+++ src/verifying/task/external_equivalence.rs
@@ -232,6 +232,74 @@ impl Task for AssembledExternalEquivalenceTask {
     type Error = ExternalEquivalenceTaskError;
 
     fn decompose(self) -> Result<Vec<Problem>, Self::Error> {
-        todo!()
+        let mut problems = Vec::new();
+
+        if matches!(
+            self.direction,
+            fol::Direction::Universal | fol::Direction::Forward
+        ) {
+            let mut axioms = self.stable_premises.clone();
+            axioms.extend(self.forward_premises.clone());
+
+            for (i, lemma) in self.proof_outline.forward_lemmas.iter().enumerate() {
+                for (j, conjecture) in lemma.conjectures.iter().enumerate() {
+                    problems.push(
+                        Problem::with_name(format!("forward_outline_{i}_{j}"))
+                            .add_annotated_formulas(axioms.clone())
+                            .add_annotated_formulas(std::iter::once(conjecture.clone())),
+                    );
+                }
+                axioms.append(&mut lemma.consequences.clone());
+            }
+
+            problems.append(
+                &mut Problem::with_name("forward_problem")
+                    .add_annotated_formulas(self.stable_premises.clone())
+                    .add_annotated_formulas(self.forward_premises)
+                    .add_annotated_formulas(
+                        self.proof_outline
+                            .forward_lemmas
+                            .into_iter()
+                            .flat_map(|g: GeneralLemma| g.consequences.into_iter()),
+                    )
+                    .add_annotated_formulas(self.forward_conclusions)
+                    .decompose(self.decomposition),
+            );
+        }
+
+        if matches!(
+            self.direction,
+            fol::Direction::Universal | fol::Direction::Backward
+        ) {
+            let mut axioms = self.stable_premises.clone();
+            axioms.extend(self.backward_premises.clone());
+
+            for (i, lemma) in self.proof_outline.backward_lemmas.iter().enumerate() {
+                for (j, conjecture) in lemma.conjectures.iter().enumerate() {
+                    problems.push(
+                        Problem::with_name(format!("backward_outline_{i}_{j}"))
+                            .add_annotated_formulas(axioms.clone())
+                            .add_annotated_formulas(std::iter::once(conjecture.clone())),
+                    );
+                }
+                axioms.append(&mut lemma.consequences.clone());
+            }
+
+            problems.append(
+                &mut Problem::with_name("backward_problem")
+                    .add_annotated_formulas(self.stable_premises)
+                    .add_annotated_formulas(self.backward_premises)
+                    .add_annotated_formulas(
+                        self.proof_outline
+                            .backward_lemmas
+                            .into_iter()
+                            .flat_map(|g: GeneralLemma| g.consequences.into_iter()),
+                    )
+                    .add_annotated_formulas(self.backward_conclusions)
+                    .decompose(self.decomposition),
+            );
+        }
+
+        Ok(problems)
     }
 }
