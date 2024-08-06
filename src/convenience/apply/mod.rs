4048e99e7317eba282cce09a98ffd568a87e354f
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2024-08-06 17:42:23 +0200

    Split simplifications into multiple functions

--- src/convenience/apply/mod.rs
+++ src/convenience/apply/mod.rs
@@ -1,9 +1,27 @@
 use crate::syntax_tree::fol::Formula;
 
 pub trait Apply {
+    /// Apply an operation `f` in post-order to each node of a tree
     fn apply(self, f: &mut impl FnMut(Self) -> Self) -> Self
     where
         Self: Sized;
+
+    /// Apply a series of operations `fs` in post-order to each node of a tree
+    ///
+    /// This function will traverse the tree only once. Whenever a node is visited, the first operation is applied first.
+    /// The remaining operations are also applied in this order.
+    fn apply_all(self, fs: &mut Vec<Box<dyn FnMut(Self) -> Self>>) -> Self
+    where
+        Self: Sized,
+    {
+        let mut f = |mut node: Self| {
+            for fi in fs.iter_mut() {
+                node = fi(node);
+            }
+            node
+        };
+        self.apply(&mut f)
+    }
 }
 
 impl Apply for Formula {
