daae007ce35f7e5b28f8b76f52f115eb5c919391
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2024-08-12 09:11:55 +0200

    Add trait for Report and Prover

--- src/verifying/proof/mod.rs
+++ src/verifying/proof/mod.rs
@@ -1,1 +1,15 @@
-// TODO Tobias: Draft something
+use {
+    crate::verifying::problem::Problem,
+    std::fmt::{Debug, Display},
+};
+
+pub mod vampire;
+
+pub trait Report: Display + Debug + Clone {}
+
+pub trait Prover {
+    type Report: Report;
+    type Error;
+
+    fn prove(&self, problem: Problem) -> Result<Self::Report, Self::Error>;
+}
