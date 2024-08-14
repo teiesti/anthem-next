1fb381c5b5abf9f1aa738e234dc2638d6fe2cf58
ZachJHansen <zachhansen@unomaha.edu>
2024-08-14 14:59:53 +0200

    adding a function to check private recursion

--- src/analyzing/tightness.rs
+++ src/analyzing/tightness.rs
@@ -1,11 +1,14 @@
 use {
-    crate::syntax_tree::asp::Program,
+    crate::syntax_tree::asp::{self, Program},
+    indexmap::IndexSet,
     petgraph::{algo::is_cyclic_directed, graph::DiGraph},
     std::collections::HashMap,
 };
 
 pub trait Tightness {
     fn is_tight(&self) -> bool;
+
+    fn private_recursion_free(&self, private_predicates: IndexSet<asp::Predicate>) -> bool;
 }
 
 impl Tightness for Program {
@@ -32,11 +35,58 @@ impl Tightness for Program {
 
         !is_cyclic_directed(&dependency_graph)
     }
+
+    fn private_recursion_free(&self, private_predicates: IndexSet<asp::Predicate>) -> bool {
+        for rule in &self.rules {
+            match rule.head.clone() {
+                asp::Head::Choice(a) => {
+                    if private_predicates.contains(&a.predicate()) {
+                        return false;
+                    }
+                }
+                asp::Head::Basic(_) | asp::Head::Falsity => (),
+            }
+        }
+
+        let mut dependency_graph = DiGraph::<(), ()>::new();
+        let mut mapping = HashMap::new();
+
+        for predicate in self.predicates() {
+            if private_predicates.contains(&predicate) {
+                let node = dependency_graph.add_node(());
+                mapping.insert(predicate, node);
+            }
+        }
+
+        for rule in &self.rules {
+            if let Some(head_predicate) = rule.head.predicate() {
+                if private_predicates.contains(&head_predicate) {
+                    let body_predicates = rule.body.predicates();
+                    let private_body_predicates = body_predicates.intersection(&private_predicates);
+
+                    for predicate in private_body_predicates {
+                        dependency_graph.update_edge(
+                            mapping[&head_predicate],
+                            mapping[&predicate],
+                            (),
+                        );
+                    }
+                }
+            }
+        }
+
+        !is_cyclic_directed(&dependency_graph)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use {super::Tightness, crate::syntax_tree::asp::Program, std::str::FromStr};
+    use {
+        super::Tightness,
+        crate::syntax_tree::{asp::Predicate, asp::Program},
+        indexmap::IndexSet,
+        std::str::FromStr,
+    };
 
     #[test]
     fn test_tightness() {
@@ -56,6 +106,33 @@ mod tests {
             "p :- q, not r. p :- r. r :- p.",
         ] {
             assert!(!Program::from_str(program).unwrap().is_tight())
+        }
+    }
+
+    #[test]
+    fn test_private_recursion() {
+        for program in ["a :- not b. b :- not a.", "a :- p(1). p(X) :- q(X)."] {
+            let a: Predicate = "a/0".parse().unwrap();
+            let p: Predicate = "p/1".parse().unwrap();
+            let private_predicates: IndexSet<Predicate> = IndexSet::from_iter([a, p]);
+            assert!(Program::from_str(program)
+                .unwrap()
+                .private_recursion_free(private_predicates))
+        }
+
+        for program in [
+            "{a}.",
+            "a :- not a.",
+            "{p(X)} :- not not p(X). b :- a.",
+            "a :- not b. b :- not a.",
+        ] {
+            let a: Predicate = "a/0".parse().unwrap();
+            let b: Predicate = "b/0".parse().unwrap();
+            let p: Predicate = "p/1".parse().unwrap();
+            let private_predicates: IndexSet<Predicate> = IndexSet::from_iter([a, b, p]);
+            assert!(!Program::from_str(program)
+                .unwrap()
+                .private_recursion_free(private_predicates))
         }
     }
 }
