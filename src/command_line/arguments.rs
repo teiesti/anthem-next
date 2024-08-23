158f2b0a44e7473a9adfa48a96877cc0805aa1ba
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2024-08-23 10:01:37 +0200

    Enhance documentation

--- src/command_line/arguments.rs
+++ src/command_line/arguments.rs
@@ -62,7 +62,7 @@ pub enum Command {
         #[arg(long, action)]
         no_proof_search: bool,
 
-        /// The time limit in seconds to prove each conjecture passed to an ATP
+        /// The time limit in seconds to prove each problem passed to a prover
         #[arg(long, short, default_value_t = 60)]
         time_limit: usize,
 
