52e80577feeb7f7a618941c97daf163638af750a
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2025-02-01 01:39:14 +0100

    Add remove_double_negation simplification

--- src/simplifying/fol/classic.rs
+++ src/simplifying/fol/classic.rs
@@ -1,6 +1,46 @@
-use crate::syntax_tree::fol::Theory;
+use crate::{
+    convenience::unbox::{fol::UnboxedFormula, Unbox as _},
+    syntax_tree::fol::{Formula, Theory, UnaryConnective},
+};
 
 pub fn simplify(theory: Theory) -> Theory {
     crate::simplifying::fol::ht::simplify(theory)
-    // TODO: Add classic simplifications
+    // TODO: Apply classic simplifications
+}
+
+pub fn remove_double_negation(formula: Formula) -> Formula {
+    // Remove double negation
+    // e.g. not not F => F
+
+    match formula.unbox() {
+        UnboxedFormula::UnaryFormula {
+            connective: UnaryConnective::Negation,
+            formula:
+                Formula::UnaryFormula {
+                    connective: UnaryConnective::Negation,
+                    formula: inner,
+                },
+        } => *inner,
+
+        x => x.rebox(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::remove_double_negation,
+        crate::{convenience::apply::Apply as _, syntax_tree::fol::Formula},
+    };
+
+    #[test]
+    fn test_eliminate_double_negation() {
+        assert_eq!(
+            "not not a"
+                .parse::<Formula>()
+                .unwrap()
+                .apply(&mut remove_double_negation),
+            "a".parse().unwrap()
+        )
+    }
 }
